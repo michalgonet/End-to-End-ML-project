464169a668f2919b4d07ff03f3a8b35295cadfaf
Michał Gonet <michal.gonet.mail@gmail.com>
2023-10-07 10:47:04 +0200

    First Stage added

---
+++ CNN_Classifier/modules/data_ingestion.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import urllib.request as request
+import zipfile
+from CNN_Classifier import logger
+from CNN_Classifier.utils.common import get_size
+from CNN_Classifier.entity.config_entity import DataIngestionConfig
+
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not Path(self.config.local_data_file).exists():
+            filename, headers = request.urlretrieve(
+                url=self.config.source_URL,
+                filename=self.config.local_data_file,
+            )
+            # TODO: THERE IS A PROBLEM WITH DOWNLOAD FILE FOR NOW THE FILE IS DOWNLOADED MANUALLY
+            logger.info(f'{filename} downloaded with following info {headers}')
+        else:
+            logger.info(f'Files already exists of size: {get_size(Path(self.config.local_data_file))}')
+
+    def extract_zip_file(self):
+        """
+        Extracts the zip file into the data directory
+        """
+        unzip_path = Path(self.config.unzip_dir)
+        unzip_path.mkdir(parents=True, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
--- CNN_Classifier/utils/common.py
+++ CNN_Classifier/utils/common.py
@@ -1,4 +1,3 @@
-import os
 import json
 import base64
 from pathlib import Path
@@ -29,7 +28,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            logger.info(f'yaml file: {path_to_yaml} loaded succesfully')
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
@@ -37,8 +36,27 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
         raise e
 
 
+# @ensure_annotations
+# def create_directories(path_to_directories: list, verbose=True) -> None:
+#     """
+#     Function create directories
+#     Parameters
+#     ----------
+#     path_to_directories
+#         list with paths
+#     verbose
+#         checker for providing logs
+#     Returns
+#     -------
+#     None
+#     """
+#     for path in path_to_directories:
+#         Path(path).mkdir(parents=True, exist_ok=True)
+#         if verbose:
+#             logger.info(f"created directory at: {path}")
+
 @ensure_annotations
-def create_directories(path_to_directories: list[str], verbose=True) -> None:
+def create_directories(path_to_directories: list, verbose: bool = True):
     """
     Function create directories
     Parameters
@@ -47,14 +65,12 @@ def create_directories(path_to_directories: list[str], verbose=True) -> None:
         list with paths
     verbose
         checker for providing logs
-    Returns
-    -------
-    None
     """
     for path in path_to_directories:
-        Path(path).mkdir(exist_ok=True)
+        # os.makedirs(path, exist_ok=True)
+        Path(path).mkdir(parents=True, exist_ok=True)
         if verbose:
-            logger.info(f'Created directory at: {path}')
+            logger.info(f"created directory at: {path}")
 
 
 @ensure_annotations
@@ -143,7 +159,8 @@ def get_size(path: Path) -> str:
     -------
     Size of the selected file in KB
     """
-    size_in_kb = round(os.path.getsize(path) / 1024)
+    size_in_bytes = path.stat().st_size
+    size_in_kb = round(size_in_bytes / 1024)
     return f"~ {size_in_kb} KB"
 
 
@@ -166,7 +183,7 @@ def decode_image(img_string: str, file_name: str) -> None:
         f.close()
 
 
-def encode_image_into_base64(cropped_image_path) -> str:
+def encode_image_into_base64(cropped_image_path: str) -> bytes:
     """
     Function convert image into base64 (string) format
     Parameters
